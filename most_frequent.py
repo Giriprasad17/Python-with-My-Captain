bc84515e966ae460d30ec847b519f88a2eb1dc80
GIRIPRASAD T G <77499350+Giriprasad17@users.noreply.github.com>
2021-06-06 13:21:07 +0530

    Create most_frequent.py

---
+++ most_frequent.py
@@ -0,0 +1,32 @@
+#python program to get the frequency of characters in a string in descending order of frequency
+#Input the string
+
+word = input("Input:Please enter a string\n")
+
+#create an empty dictionary
+           
+dic = {}
+           
+#getting characters from string
+
+for ch in word:
+    if ch in dic:
+           dic[ch]+=1      #if character is already present in dict increse its count by one
+    else:
+           dic[ch]=1      #else assing it for the first time in the dictionary
+           
+lis = list(dic.items())    #convert given dictionary into list
+
+# take second element for sort
+def tf(elem):
+    return elem[1]
+
+lis.sort(key=tf,reverse = True)                 #sort the letters in ascending order
+
+print("Output:\n")
+for i in range (0,len(lis)):
+    for j in range(0,2):
+          if j%2==0:
+                print(lis[i][j],end=" = ")
+          else:
+                print(lis[i][j])
